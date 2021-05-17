0fca0e36b61a134c5508dfe191f6453341cd86b5
Sudhir Mitharwal <6812992+sudkumar@users.noreply.github.com>
2021-05-17 17:26:03 +0530

    docs(api): type hint the exceptions

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,6 +3,8 @@
  * @param string expression - The expression to evaluate ("24 + 65 * 3 - 48")
  * @return number - The evaluated value.
  *
+ * @throws Will throw an error if the expression in invalid.
+ * 
  * NOTE: When passing floating points, please handle the digits after decimal point
  * e.g. 2 - 1.1 leads to 0.8999999999999999 intead of .9
  */
